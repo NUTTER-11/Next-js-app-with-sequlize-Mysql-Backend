283d63779ae7bc33ca690c05aa43d616e062ac43
NUTTER-11 <parulp8800@gmail.com>
2024-09-10 09:54:03 +0530

    2nd commit

--- 07-09-24/backend/server.js
+++ 07-09-24/backend/server.js
@@ -71,16 +71,20 @@ server.get('/search/get-all-users', async (req, res) => {
       return res.status(400).json({ message: "Enter name" });
     }
 
-    const user = await User.findOne({ where: { first_name } });
+    // Fetch user by first_name and select specific fields
+    const user = await User.findOne({
+      where: { first_name },
+      attributes: ['first_name', 'email', ]
+    });
 
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
-      
+      return res.status(404).json({ message: "User not found" });
     }
-    return res.status(201).json({user});
+
+    return res.status(200).json({ user });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Error logging in" });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
